ca8e33e44339842fd6a27d6fc39ea7d015b6a94b
g8y3e <valentine.pavchuk@ironsrc.com>
2016-12-14 20:27:50 +0200

    fixed travis build

--- tracker_test_status.sh
+++ tracker_test_status.sh
@@ -1,6 +1,8 @@
 #!/bin/bash "
-T1=$(cat logcat.txt | grep "All tasks done")
-if [ -z "$T1" ]; then
+LOG_DATA=$(cat logcat.txt)
+echo $LOG_DATA
+RESULT=$(cat logcat.txt | grep "All tasks done")
+if [ -z "$RESULT" ]; then
     echo "Found error in Tracker tests!"
     exit 1
 fi
