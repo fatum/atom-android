23ffa60291ed83b271e17735425c5b4af7010f32
Nati Dykstein <nati.dykstein@ironsrc.com>
2017-02-08 14:41:00 +0200

    fixed compile errors

--- trackertest/src/main/java/io/ironsourceatom/trackertest/TestsUtils.java
+++ trackertest/src/main/java/io/ironsourceatom/trackertest/TestsUtils.java
@@ -1,6 +1,7 @@
 package io.ironsourceatom.trackertest;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 
 import org.json.JSONArray;
@@ -15,181 +16,194 @@ import java.util.Map;
 import java.util.TreeMap;
 
 import io.ironsourceatom.sdk.RemoteConnection;
-import io.ironsourceatom.sdk.RemoteService;
 import io.ironsourceatom.sdk.Report;
-import io.ironsourceatom.sdk.ReportIntent;
+import io.ironsourceatom.sdk.ReportData;
+
 
 public class TestsUtils {
-    static class MockReport implements Report {
-        @Override
-        public void send() {
-        }
 
-        @Override
-        public MockReport setData(String value) {
-            return this;
-        }
+	static class MockReport
+			implements Report {
 
-        @Override
-        public MockReport setTable(String table) {
-            return this;
-        }
+		@Override
+		public MockReport setData(String value) {
+			return this;
+		}
 
-        @Override
-        public MockReport setToken(String token) {
-            return this;
-        }
+		@Override
+		public MockReport setTable(String table) {
+			return this;
+		}
 
-        @Override
-        public Report setEndPoint(String endpoint) {
-            return null;
-        }
+		@Override
+		public MockReport setToken(String token) {
+			return this;
+		}
 
-        @Override
-        public Report setBulk(boolean b) {
-            return null;
-        }
+		@Override
+		public Report setEndPoint(String endpoint) {
+			return null;
+		}
 
-        @Override
-        public Intent getIntent() {
-            return null;
-        }
+		@Override
+		public Report setBulk(boolean b) {
+			return null;
+		}
 
-        public int mType;
-    }
+		@Override
+		public Bundle getExtras() {
+			return null;
+		}
 
-    static class MockPoster implements RemoteConnection {
-        static final String TAG = "TrackerTest";
+		public int mType;
+	}
 
-        public static final Map<String, Boolean> TRACKER_TASKS;
+	static class MockPoster
+			implements RemoteConnection {
 
-        static int TRACKER_ERROR_400_COUNT = 0;
-        static int TRACKER_ERROR_503_COUNT = 0;
+		static final String TAG = "TrackerTest";
 
-        static
-        {
-            TRACKER_TASKS = new TreeMap<String, Boolean>();
+		public static final Map<String, Boolean> TRACKER_TASKS;
 
-            TRACKER_TASKS.put("TRACKNOW_API18", false);
-            TRACKER_TASKS.put("TRACKNOW_API21", false);
+		static int TRACKER_ERROR_400_COUNT = 0;
+		static int TRACKER_ERROR_503_COUNT = 0;
 
-            TRACKER_TASKS.put("TRACK_BULK_SIZE_API21", false);
+		static {
+			TRACKER_TASKS = new TreeMap<String, Boolean>();
 
-            TRACKER_TASKS.put("TRACK_TIMER_API21", false);
-            TRACKER_TASKS.put("TRACK_TIMER_API18", false);
+			TRACKER_TASKS.put("TRACKNOW_API18", false);
+			TRACKER_TASKS.put("TRACKNOW_API21", false);
 
-            TRACKER_TASKS.put("TRACK_400_ERROR_API21", false);
+			TRACKER_TASKS.put("TRACK_BULK_SIZE_API21", false);
 
-            TRACKER_TASKS.put("TRACK_503_ERROR_API21", false);
-        }
+			TRACKER_TASKS.put("TRACK_TIMER_API21", false);
+			TRACKER_TASKS.put("TRACK_TIMER_API18", false);
 
-        private static void setTaskStatus(String name, Boolean status) {
-            TRACKER_TASKS.put(name, status);
+			TRACKER_TASKS.put("TRACK_400_ERROR_API21", false);
 
-            printTrackerTaskStatus();
-        }
+			TRACKER_TASKS.put("TRACK_503_ERROR_API21", false);
+		}
 
-        public static void printTrackerTaskStatus() {
-            StringBuilder taskStatus = new StringBuilder();
-            taskStatus.append("Tracker tasks:\n");
-            for (Map.Entry<String, Boolean> entry : TRACKER_TASKS.entrySet()) {
-                taskStatus.append("  - ").append(entry.getKey()).append(": ").append(entry.getValue()).append(";\n");
-            }
-            taskStatus.append("\n");
+		private static void setTaskStatus(String name, Boolean status) {
+			TRACKER_TASKS.put(name, status);
 
-            Log.i(TAG, taskStatus.toString());
-        }
+			printTrackerTaskStatus();
+		}
 
-        public static boolean isAllTrackerTasksCompleted() {
-            for (Map.Entry<String, Boolean> entry : TRACKER_TASKS.entrySet()) {
-                if (!entry.getValue()) {
-                    return false;
-                }
-            }
+		public static void printTrackerTaskStatus() {
+			StringBuilder taskStatus = new StringBuilder();
+			taskStatus.append("Tracker tasks:\n");
+			for (Map.Entry<String, Boolean> entry : TRACKER_TASKS.entrySet()) {
+				taskStatus.append("  - ")
+				          .append(entry.getKey())
+				          .append(": ")
+				          .append(entry.getValue())
+				          .append(";\n");
+			}
+			taskStatus.append("\n");
 
-            return true;
-        }
+			Log.i(TAG, taskStatus.toString());
+		}
 
-        public void checkRequest(String data) throws IOException {
-            try {
-                JSONObject jsonObject = new JSONObject(data);
+		public static boolean isAllTrackerTasksCompleted() {
+			for (Map.Entry<String, Boolean> entry : TRACKER_TASKS.entrySet()) {
+				if (!entry.getValue()) {
+					return false;
+				}
+			}
 
-                String apiLevel = jsonObject.getString("data");
-                String taskName = jsonObject.getString("table");
+			return true;
+		}
 
-                if (TRACKER_TASKS.containsKey(taskName)) {
-                    if (taskName.equals("TRACK_400_ERROR_API21")) {
-                        if (TRACKER_ERROR_400_COUNT++ >= 1) {
-                            setNext(200);
-                            setTaskStatus(taskName, false);
-                        } else {
-                            setNext(400);
-                            setTaskStatus(taskName, true);
-                        }
-                    } else if (taskName.equals("TRACK_503_ERROR_API21")) {
-                        if (TRACKER_ERROR_503_COUNT++ >= 3) {
-                            setNext(200);
-                            setTaskStatus(taskName, true);
-                        } else {
-                            setNext(503);
-                        }
-                    } else {
-                        setTaskStatus(taskName, true);
-                        setNext(200);
-                    }
-                }
-            } catch (JSONException ex) {
-            }
-        }
+		public void checkRequest(String data) throws
+				IOException {
+			try {
+				JSONObject jsonObject = new JSONObject(data);
 
-        @Override
-        public Response post(String data, String url) throws IOException {
-            checkRequest(data);
+				String apiLevel = jsonObject.getString("data");
+				String taskName = jsonObject.getString("table");
 
-            Response res = new Response();
-            if (mCode == 200) {
-                try {
-                    JSONObject event = new JSONObject(data);
-                    String table = event.getString(ReportIntent.TABLE);
-                    if (!mBackedMock.containsKey(table)) {
-                        mBackedMock.put(table, new ArrayList<String>());
-                    }
-                    mBackedMock.get(table).add(data);
-                    res.code = 200;
-                    res.body = "OK";
-                } catch (JSONException e) {
-                    res.code = 400;
-                    res.body = "invalid JSON";
-                }
-            } else if (mCode == 400) {
-                res.code = 400;
-                res.body = "invalid JSON";
-            } else {
-                res.code = 503;
-            }
-            return res;
-        }
+				if (TRACKER_TASKS.containsKey(taskName)) {
+					if (taskName.equals("TRACK_400_ERROR_API21")) {
+						if (TRACKER_ERROR_400_COUNT++ >= 1) {
+							setNext(200);
+							setTaskStatus(taskName, false);
+						}
+						else {
+							setNext(400);
+							setTaskStatus(taskName, true);
+						}
+					}
+					else if (taskName.equals("TRACK_503_ERROR_API21")) {
+						if (TRACKER_ERROR_503_COUNT++ >= 3) {
+							setNext(200);
+							setTaskStatus(taskName, true);
+						}
+						else {
+							setNext(503);
+						}
+					}
+					else {
+						setTaskStatus(taskName, true);
+						setNext(200);
+					}
+				}
+			} catch (JSONException ex) {
+			}
+		}
+
+		@Override
+		public Response post(String data, String url) throws
+				IOException {
+			checkRequest(data);
+
+			Response res = new Response();
+			if (mCode == 200) {
+				try {
+					JSONObject event = new JSONObject(data);
+					String table = event.getString(ReportData.TABLE);
+					if (!mBackedMock.containsKey(table)) {
+						mBackedMock.put(table, new ArrayList<String>());
+					}
+					mBackedMock.get(table)
+					           .add(data);
+					res.code = 200;
+					res.body = "OK";
+				} catch (JSONException e) {
+					res.code = 400;
+					res.body = "invalid JSON";
+				}
+			}
+			else if (mCode == 400) {
+				res.code = 400;
+				res.body = "invalid JSON";
+			}
+			else {
+				res.code = 503;
+			}
+			return res;
+		}
 
 
-        public MockPoster setNext(int code) {
-            this.mCode = code;
-            return this;
-        }
+		public MockPoster setNext(int code) {
+			this.mCode = code;
+			return this;
+		}
 
-        // Hack to ignore keys ordering
-        public String get(String key) {
-            JSONArray events = new JSONArray();
-            for (String event : mBackedMock.get(key)) {
-                try {
-                    events.put(new JSONObject(event));
-                } catch (JSONException e) {
-                }
-            }
-            return events.toString();
-        }
+		// Hack to ignore keys ordering
+		public String get(String key) {
+			JSONArray events = new JSONArray();
+			for (String event : mBackedMock.get(key)) {
+				try {
+					events.put(new JSONObject(event));
+				} catch (JSONException e) {
+				}
+			}
+			return events.toString();
+		}
 
-        // catch all incoming requests
-        final public Map<String, List<String>> mBackedMock = new HashMap<String, List<String>>();
-        private int mCode = 200;
-    }
+		// catch all incoming requests
+		final public Map<String, List<String>> mBackedMock = new HashMap<String, List<String>>();
+		private      int                       mCode       = 200;
+	}
 }
