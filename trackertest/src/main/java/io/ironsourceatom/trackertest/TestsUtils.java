ff6465068182acfe5338c5c44e118bde8ef808b0
Nati Dykstein <nati.dykstein@ironsrc.com>
2017-02-08 12:11:41 +0200

    Removed redundant code from tracker test

--- trackertest/src/main/java/io/ironsourceatom/trackertest/TestsUtils.java
+++ trackertest/src/main/java/io/ironsourceatom/trackertest/TestsUtils.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+import io.ironsourceatom.sdk.RemoteConnection;
 import io.ironsourceatom.sdk.RemoteService;
 import io.ironsourceatom.sdk.Report;
 import io.ironsourceatom.sdk.ReportIntent;
@@ -57,7 +58,7 @@ public class TestsUtils {
         public int mType;
     }
 
-    static class MockPoster implements RemoteService {
+    static class MockPoster implements RemoteConnection {
         static final String TAG = "TrackerTest";
 
         public static final Map<String, Boolean> TRACKER_TASKS;
